816a130cefc1509d3e52fe469c4ff208c90ad833
korma <korma@JAKUBPC>
2024-07-04 13:26:56 +0200

    FE a BE initial commit

---
+++ api/nia_api/Requests/SendingVerificationRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nia_api.Requests;
+
+public class ForgotPasswordRequest
+{
+    [Required, EmailAddress, Display(Name = "Registered email address")]
+    public string Email { get; set; }
+    public bool EmailSent { get; set; }
+}
