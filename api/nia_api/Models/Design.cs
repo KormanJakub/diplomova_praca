b3b117ca6341ae5da979b8ed4eec0143dc5ed59f
korma <korma@JAKUBPC>
2024-08-29 22:57:29 +0200

    29.8. commit

---
+++ api/nia_api/Models/Design.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson;
+
+namespace nia_api.Models;
+
+public class Designň
+{
+    [BsonId(IdGenerator = typeof(AscendingGuidGenerator))]
+    [BsonElement("_id") ,BsonRepresentation(BsonType.String)]
+    public Guid Id { get; set; }
+    [BsonElement("name"), BsonRepresentation(BsonType.String)]
+    public string? Name { get; set; }
+    [BsonElement("description"), BsonRepresentation(BsonType.String)]
+    public string? Description { get; set; }
+    [BsonElement("price"), BsonRepresentation(BsonType.Double)]
+    public double Price {  get; set; }
+    /*
+     * TODO: Pridať tagy
+     * TODO: Pridať na danú vec údaje (napr. mikina cervená, toľko kusov)
+     */
+    [BsonElement("createdAt"), BsonRepresentation(BsonType.DateTime)]
+    public DateTime? CreatedAt { get; set; }
+    [BsonElement("updatedAt"), BsonRepresentation(BsonType.DateTime)]
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ api/nia_api/Models/PairedDesing.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson;
+
+namespace nia_api.Models;
+
+public class PairedDesings
+{
+    [BsonId(IdGenerator = typeof(AscendingGuidGenerator))]
+    [BsonElement("_id") ,BsonRepresentation(BsonType.String)]
+    public Guid Id { get; set; }
+    [BsonElement("designId")]
+    public List<Design> DesignIds { get; set; } = new List<Design>();
+}
