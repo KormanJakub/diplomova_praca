7512d2c187c62b6deb8ce293bad7568123ad5b20
korma <korma@JAKUBPC>
2024-11-19 14:40:39 +0100

    19.11.2024 commit

---
+++ api/nia_api/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using nia_api.Data;
+using nia_api.Models;
+
+namespace nia_api.Controllers;
+
+[ApiController]
+[Route("payment")]
+public class PaymentController : ControllerBase
+{
+    private readonly IMongoCollection<Product> _products;
+
+    public PaymentController(NiaDbContext context)
+    {
+        _products = context.Products;
+    }
+    
+    
+
+}
---
+++ api/nia_api/Requests/GuestOrderRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace nia_api.Requests;
+
+public class OrderRequest
+{
+    public List<string> CustomizationId { get; set; }
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Country { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public string? Zip { get; set; }
+}
