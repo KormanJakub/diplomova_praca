573e6a4bf8bfecc3f4a47a13a4717f2f1d345d0a
korma <korma@JAKUBPC>
2025-02-11 16:33:47 +0100

    11.2. commit of FE - home page

--- web/src/app/Components/home/home.component.ts
+++ web/src/app/Components/home/home.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
 import {CarouselModule} from "primeng/carousel";
 import {FirstCardComponent} from "../home-page/first-card/first-card.component";
+import {BestSaleComponent} from "../home-page/best-sale/best-sale.component";
+import {SecondCardComponent} from "../home-page/second-card/second-card.component";
+import {ThirdCardComponent} from "../home-page/third-card/third-card.component";
+import {GaleryCardComponent} from "../home-page/galery-card/galery-card.component";
+import {ReviewCardComponent} from "../home-page/review-card/review-card.component";
+import {StepCardComponent} from "../home-page/step-card/step-card.component";
+import {StartCardComponent} from "../home-page/start-card/start-card.component";
+import {QuestionCardComponent} from "../home-page/question-card/question-card.component";
+import {QuestionsByCardComponent} from "../home-page/questions-by-card/questions-by-card.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +19,17 @@ import {FirstCardComponent} from "../home-page/first-card/first-card.component";
   imports: [
     HeaderComponent,
     CarouselModule,
-    FirstCardComponent
+    FirstCardComponent,
+    BestSaleComponent,
+    SecondCardComponent,
+    ThirdCardComponent,
+    GaleryCardComponent,
+    ReviewCardComponent,
+    StepCardComponent,
+    StartCardComponent,
+    QuestionCardComponent,
+    QuestionsByCardComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
