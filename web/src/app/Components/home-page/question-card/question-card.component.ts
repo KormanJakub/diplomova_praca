d12b15eda12e7bf30cc08c8ad7b40b3193b6e5cf
korma <korma@JAKUBPC>
2025-02-12 16:52:57 +0100

    Commit NIA_1: Initial commit of FE Home Page. Page doesn't have footer. (12.2.2025)

--- web/src/app/Components/home-page/question-card/question-card.component.ts
+++ web/src/app/Components/home-page/question-card/question-card.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {FloatLabelModule} from "primeng/floatlabel";
+import {InputTextModule} from "primeng/inputtext";
 
 @Component({
   selector: 'app-question-card',
   standalone: true,
-  imports: [],
+  imports: [
+    FloatLabelModule,
+    InputTextModule
+  ],
   templateUrl: './question-card.component.html',
   styleUrl: './question-card.component.css'
 })
