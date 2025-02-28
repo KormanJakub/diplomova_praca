2d40461aafca44e93fab41f5de3828a8185d9dcd
korma <korma@JAKUBPC>
2025-02-28 21:51:30 +0100

    Commit NIA_6: Admin page commit, User page commit. (28.2.2025)

---
+++ web/src/app/Components/user-page/user-left-navigate/user-left-navigate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-left-navigate',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-left-navigate.component.html',
+  styleUrl: './user-left-navigate.component.css'
+})
+export class UserLeftNavigateComponent {
+
+}
---
+++ web/src/app/Components/user-page/user-order-info/user-order-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-order-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-order-info.component.html',
+  styleUrl: './user-order-info.component.css'
+})
+export class UserOrderInfoComponent {
+
+}
