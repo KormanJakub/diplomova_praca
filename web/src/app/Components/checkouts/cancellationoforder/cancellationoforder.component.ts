797aadb3fd167979cbebfbc5cf3072d5b886ae7a
korma <korma@JAKUBPC>
2025-03-29 15:22:35 +0100

    Commit NIA_8: Success payment and cancel order commit. Fixed payment service on backend. (29.3.2025)

---
+++ web/src/app/Components/checkouts/cancellationoforder/cancellationoforder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancellationoforder',
+  standalone: true,
+  imports: [],
+  templateUrl: './cancellationoforder.component.html',
+  styleUrl: './cancellationoforder.component.css'
+})
+export class CancellationoforderComponent {
+
+}
