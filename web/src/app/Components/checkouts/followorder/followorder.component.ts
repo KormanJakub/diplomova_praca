c8afe2cc8c7c9d4515e55dd0ecbeabc82d4a8a50
korma <korma@JAKUBPC>
2025-03-30 19:05:25 +0200

    Commit NIA_10: Guest user can now make order. Change of FE for first-card-component. Added following of order on BE and FE. (30.3.2025)

---
+++ web/src/app/Components/checkouts/followorder/followorder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-followorder',
+  standalone: true,
+  imports: [],
+  templateUrl: './followorder.component.html',
+  styleUrl: './followorder.component.css'
+})
+export class FolloworderComponent {
+
+}
